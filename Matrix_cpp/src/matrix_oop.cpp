aa54afa4bfc3ae290d9cebe85714b0db8e939320
AlexanderShilov <scrib.ru@mail.ru>
2022-10-12 08:08:00 +0700

    release 1.0

---
+++ Matrix_cpp/src/matrix_oop.cpp
@@ -0,0 +1,430 @@
+#include "matrix_oop.h"
+
+// Выделение памяти
+void Matrix::MemoryAllocation(const int rows, const int cols) {
+  if (rows >= 0 && cols >= 0) {
+    matrix_ = new double*[rows];
+    for (int i = 0; i < rows; i++) {
+      matrix_[i] = new double[cols]{};
+    }
+  }
+}
+
+// Выделение памяти для копирования
+void Matrix::MemoryAllocation(const Matrix& other) {
+  if (rows_ >= 0 && cols_ >= 0) {
+    matrix_ = new double*[rows_];
+    for (int i = 0; i < rows_; i++) {
+      matrix_[i] = new double[cols_];
+      for (int j = 0; j < cols_; j++) {
+        matrix_[i][j] = other.matrix_[i][j];
+      }
+    }
+  }
+}
+
+// Копирование элементов матрицы
+void Matrix::CopyElementMatrix(const Matrix& temp, const int rows,
+                                  const int cols) {
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < cols; j++) {
+      matrix_[i][j] = temp.matrix_[i][j];
+    }
+  }
+}
+
+// Очистка памяти
+void Matrix::FreeMemory() {
+  if (matrix_) {
+    for (int i = 0; i < rows_; i++) {
+      delete[] matrix_[i];
+    }
+    delete[] matrix_;
+    matrix_ = nullptr;
+  }
+}
+
+// Ошибка размерности матриц
+void Matrix::SizeError(const Matrix& other) const {
+  if (rows_ != other.rows_ || cols_ != other.cols_) {
+    throw std::invalid_argument("Error, different dimension of matrices");
+  }
+}
+
+// Ошибка пустой матрицы
+void Matrix::EmptyMatrixError() const {
+  if (matrix_ == nullptr) {
+    throw std::invalid_argument("Error, incorect matrix");
+  }
+}
+
+// Ошибка пустых матриц
+void Matrix::EmptyMatrixError(const Matrix& other) const {
+  if (matrix_ == nullptr || other.matrix_ == nullptr) {
+    std::invalid_argument("Error, incorect matrix");
+  }
+}
+
+// Приведение матрицы к треугольной
+void Matrix::ReducingMatrixTriangular(const Matrix& temp) const {
+  double number_zero;
+  for (int i = 0; i < temp.rows_; i++) {
+    if (!temp.matrix_[i][i]) {
+      for (int j = i + 1; j < rows_; j++) {
+        for (int k = 0; k < rows_; k++) {
+          temp.matrix_[i][k] += temp.matrix_[j][k];
+        }
+      }
+    }
+    if (temp.matrix_[i][i]) {
+      for (int j = i + 1; j < temp.rows_; j++) {
+        number_zero = temp.matrix_[j][i] / temp.matrix_[i][i];
+        for (int k = 0; k < temp.rows_; k++) {
+          temp.matrix_[j][k] -= temp.matrix_[i][k] * number_zero;
+        }
+      }
+    }
+  }
+}
+
+// уменьшение матриц
+void Matrix::MatrixReduction(const int r, const int c,
+                                Matrix& matr_reduction) const {
+  int x = 0;
+  for (int i = 0; i < rows_; i++) {
+    if (r != i) {
+      int y = 0;
+      for (int j = 0; j < cols_; j++) {
+        if (c != j) {
+          matr_reduction.matrix_[x][y] = matrix_[i][j];
+          y++;
+        }
+      }
+      x++;
+    }
+  }
+}
+
+// Базовый конструктор
+Matrix::Matrix() : rows_(0), cols_(0), matrix_(nullptr) {}
+
+// Параметризированный конструктор
+Matrix::Matrix(int rows, int cols) : rows_(cols), cols_(rows) {
+  if (rows <= 0 || cols <= 0) {
+    throw std::invalid_argument(
+        "Error, rows and cols must be greater than zero");
+  }
+  MemoryAllocation(rows_, cols_);
+}
+
+// Параметризированный конструктор переносит значения из массива в матрицу
+Matrix::Matrix(int rows, int cols, const double data[]) {
+  if (rows > 0 && cols > 0) {
+    rows_ = rows;
+    cols_ = cols;
+    matrix_ = new double*[rows_];
+    int k = 0;
+    for (int i = 0; i < rows_; i++) {
+      matrix_[i] = new double[cols_];
+      for (int j = 0; j < cols_; j++) {
+        matrix_[i][j] = data[k];
+        k++;
+      }
+    }
+  }
+}
+
+// Конструктор копирования
+Matrix::Matrix(const Matrix& other)
+    : rows_(other.rows_), cols_(other.cols_) {
+  MemoryAllocation(other);
+}
+
+// Конструктор переноса
+Matrix::Matrix(Matrix&& other)
+    : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
+  other.rows_ = 0;
+  other.cols_ = 0;
+  other.matrix_ = nullptr;
+}
+
+// Деструктор
+Matrix::~Matrix() { FreeMemory(); }
+
+// Перегрузка оператора '+'
+Matrix Matrix::operator+(const Matrix& other) const {
+  Matrix result(*this);
+  result.SumMatrix(other);
+  return result;
+}
+
+// Перегрузка оператора '-'
+Matrix Matrix::operator-(const Matrix& other) const {
+  Matrix result(*this);
+  result.SubMatrix(other);
+  return result;
+}
+
+// Перегрузка оператора '*'
+Matrix Matrix::operator*(const Matrix& other) const {
+  Matrix result(*this);
+  result.MulMatrix(other);
+  return result;
+}
+
+// Перегрузка оператора '*'
+Matrix operator*(const double num, const Matrix& other) {
+  Matrix result(other);
+  result.MulNumber(num);
+  return result;
+}
+
+// Перегрузка оператора '*' для числа
+Matrix Matrix::operator*(const double num) const {
+  Matrix result(*this);
+  result.MulNumber(num);
+  return result;
+}
+
+// Перегрузка оператора '=='
+bool Matrix::operator==(const Matrix& other) const {
+  bool result = EqMatrix(other);
+  return result;
+}
+
+// Перегрузка оператора '='
+Matrix& Matrix::operator=(const Matrix& other) {
+  FreeMemory();
+  rows_ = other.rows_;
+  cols_ = other.cols_;
+  MemoryAllocation(other);
+  return *this;
+}
+
+// Перегрузка оператора '+='
+void Matrix::operator+=(const Matrix& other) { SumMatrix(other); }
+
+// Перегрузка оператора '-='
+void Matrix::operator-=(const Matrix& other) { SubMatrix(other); }
+
+// Перегрузка оператора '*='
+void Matrix::operator*=(const Matrix& other) { MulMatrix(other); }
+
+// Перегрузка оператора '*=' для числа
+void Matrix::operator*=(const double num) { MulNumber(num); }
+
+// Перегрузка оператора индексации
+double& Matrix::operator()(int i, int j) {
+  EmptyMatrixError();
+  if (i < 0 || i >= rows_ || j < 0 || j >= cols_) {
+    throw std::out_of_range("Error, index incorrect");
+  }
+  return matrix_[i][j];
+}
+
+// Перегрузка оператора индексации
+double Matrix::operator()(int i, int j) const {
+  EmptyMatrixError();
+  if (i < 0 || i >= rows_ || j < 0 || j >= cols_) {
+    throw std::out_of_range("Error, index incorrect");
+  }
+  return matrix_[i][j];
+}
+
+// Изменение строки
+void Matrix::SetRows(int rows) {
+  if (rows <= 0) {
+    throw std::invalid_argument("Error, the string cannot be less than zero");
+  }
+
+  if (rows != rows_) {
+    Matrix temp(*this);
+    this->~Matrix();
+
+    // создание новой матрицы и заполнение нулями
+    MemoryAllocation(rows, cols_);
+
+    if (rows >= rows_) {
+      CopyElementMatrix(temp, rows_, cols_);
+    } else {
+      CopyElementMatrix(temp, rows, cols_);
+    }
+    rows_ = rows;
+  }
+}
+
+// Изменение колонки
+void Matrix::SetCols(int cols) {
+  if (cols <= 0) {
+    throw std::invalid_argument("Error, the string cannot be less than zero");
+  }
+
+  if (cols != cols_) {
+    Matrix temp(*this);
+    this->~Matrix();
+
+    // создание новой матрицы и заполнение нулями
+    MemoryAllocation(rows_, cols);
+
+    if (cols >= cols_) {
+      CopyElementMatrix(temp, rows_, cols_);
+    } else {
+      CopyElementMatrix(temp, rows_, cols);
+    }
+    cols_ = cols;
+  }
+}
+
+// Получение строки
+int Matrix::GetRows() const { return rows_; }
+
+// Получение колонки
+int Matrix::GetCols() const { return cols_; }
+
+// Сравнение матриц
+bool Matrix::EqMatrix(const Matrix& other) const {
+  bool result = true;
+  if (rows_ == other.rows_ && cols_ == other.cols_ && matrix_ &&
+      other.matrix_) {
+    for (int i = 0; i < rows_; i++) {
+      for (int j = 0; j < cols_; j++) {
+        if (fabs(matrix_[i][j] - other.matrix_[i][j]) > ACCURACY) {
+          result = false;
+          break;
+        }
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+}
+
+// Сложение матриц
+void Matrix::SumMatrix(const Matrix& other) {
+  EmptyMatrixError(other);
+  SizeError(other);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] += other.matrix_[i][j];
+    }
+  }
+}
+
+// Вычитание матриц
+void Matrix::SubMatrix(const Matrix& other) {
+  EmptyMatrixError(other);
+  SizeError(other);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] -= other.matrix_[i][j];
+    }
+  }
+}
+
+// Умножение матрицы на число
+void Matrix::MulNumber(const double num) {
+  EmptyMatrixError();
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] *= num;
+    }
+  }
+}
+
+// Перемножение матриц
+void Matrix::MulMatrix(const Matrix& other) {
+  if (cols_ != other.rows_) {
+    throw std::invalid_argument(
+        "Error, the number of columns of the first matrix is \
+                          not equal to the number of rows of the second matrix");
+  }
+  EmptyMatrixError(other);
+  Matrix res(rows_, other.cols_);
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < other.cols_; j++) {
+      res.matrix_[i][j] = 0;
+      for (int k = 0; k < cols_; k++) {
+        res.matrix_[i][j] += matrix_[i][k] * other.matrix_[k][j];
+      }
+    }
+  }
+  *this = res;
+}
+
+// Транспортирование матрицы
+Matrix Matrix::Transpose() const {
+  if (matrix_ == nullptr) {
+    throw std::invalid_argument("Error, incorect matrix");
+  }
+  Matrix result(rows_, cols_);
+  for (int i = 0; i < cols_; i++) {
+    for (int j = 0; j < rows_; j++) {
+      result.matrix_[i][j] = matrix_[j][i];
+    }
+  }
+  return result;
+}
+
+// Минор матрицы и матрица алгебраических дополнений
+Matrix Matrix::CalcComplements() const {
+  EmptyMatrixError();
+  if (cols_ != rows_) {
+    throw std::invalid_argument("Error, the matrix is not square");
+  }
+  Matrix result(rows_, cols_);
+  if (rows_ == 1) {
+    result.matrix_[0][0] = 1;
+  } else {
+    Matrix reducation(rows_ - 1, cols_ - 1);
+    for (int i = 0; i < rows_; i++) {
+      for (int j = 0; j < cols_; j++) {
+        MatrixReduction(i, j, reducation);
+        double d = reducation.Determinant();
+        result.matrix_[i][j] = pow(-1., (double)(i + j)) * d;
+      }
+    }
+  }
+  return result;
+}
+
+// Определитель матрицы
+double Matrix::Determinant() const {
+  EmptyMatrixError();
+  if (cols_ != rows_) {
+    throw std::invalid_argument("Error, the matrix is not square");
+  }
+  Matrix temp(*this);
+  double det = 1;
+  if (rows_ == 1) {
+    det = temp.matrix_[0][0];
+  } else {
+    ReducingMatrixTriangular(temp);
+    for (int i = 0; i < rows_; i++) {
+      det *= temp.matrix_[i][i];
+    }
+  }
+  return det;
+}
+
+// Инверсия матрицы
+Matrix Matrix::InverseMtrix() const {
+  EmptyMatrixError();
+  if (rows_ < 2) {
+    throw std::invalid_argument("Erorr, incorrect matrix");
+  }
+  Matrix result(rows_, cols_);
+  double det = Determinant();
+
+  if (det == 0) {
+    throw std::invalid_argument("Erorr, the determinant of the matrix is ZERO");
+  }
+
+  Matrix tempMatr = CalcComplements().Transpose();
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      result.matrix_[i][j] = tempMatr.matrix_[i][j] / det;
+    }
+  }
+  return result;
+}
---
+++ Matrix_cpp/src/matrix_oop.h
@@ -0,0 +1,77 @@
+#ifndef MATRIX_MATRIX_OOP_
+#define MATRIX_MATRIX_OOP_
+
+#include <math.h>
+
+#include <exception>
+#include <iostream>
+
+#define ACCURACY 1e-7
+
+class Matrix {
+ private:
+  int rows_, cols_;
+  double** matrix_ = nullptr;
+
+  void MemoryAllocation();
+  void MemoryAllocation(const int rows, const int cols);
+  void MemoryAllocation(const Matrix& other);
+  void CopyElementMatrix(const Matrix& temp, const int rows, const int cols);
+  void FreeMemory();
+  void SizeError(const Matrix& other) const;
+  void EmptyMatrixError() const;
+  void EmptyMatrixError(const Matrix& other) const;
+  void ReducingMatrixTriangular(const Matrix& temp) const;
+  void MatrixReduction(const int r, const int c,
+                       Matrix& result_reduction) const;
+
+ public:
+  // Конструкторы и деструктор
+
+  Matrix();
+  Matrix(int rows, int cols);
+  Matrix(int rows, int cols, const double data[]);
+  Matrix(const Matrix& other);
+  Matrix(Matrix&& other);
+  ~Matrix();
+
+  // Перегрузка операторов
+
+  Matrix operator+(const Matrix& other) const;
+  Matrix operator-(const Matrix& other) const;
+  Matrix operator*(const Matrix& other) const;
+  friend Matrix operator*(const double num, const Matrix& other);
+  Matrix operator*(const double num) const;
+  bool operator==(const Matrix& other) const;
+  Matrix& operator=(const Matrix& other);
+  void operator+=(const Matrix& other);
+  void operator-=(const Matrix& other);
+  void operator*=(const Matrix& other);
+  void operator*=(const double num);
+  double& operator()(int i, int j);
+  double operator()(int i, int j) const;
+
+  // Сеттеры
+
+  void SetRows(int rows);
+  void SetCols(int cols);
+
+  // Геттеры
+
+  int GetRows() const;
+  int GetCols() const;
+
+  // Методы класса
+
+  bool EqMatrix(const Matrix& other) const;
+  void SumMatrix(const Matrix& other);
+  void SubMatrix(const Matrix& other);
+  void MulNumber(const double num);
+  void MulMatrix(const Matrix& other);
+  Matrix Transpose() const;
+  Matrix CalcComplements() const;
+  double Determinant() const;
+  Matrix InverseMtrix() const;
+};
+
+#endif  // MATRIX__MATRIX_OOP_
